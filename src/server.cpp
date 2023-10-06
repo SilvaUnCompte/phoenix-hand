5681fc436381e0192645c736deade7d01637bac1
SilvaUnCompte <silvablocks@gmail.com>
2023-10-06 10:06:13 +0200

    Dlete useless code in server.h

--- src/server.cpp
+++ src/server.cpp
@@ -9,12 +9,6 @@ static const char *JPG_CONTENT_TYPE = "image/jpeg";
 
 AsyncWebServer server(80);
 
-#define THUMB_PIN 0
-#define INDEX_PIN 1
-#define MIDDLE_PIN 2
-#define RING_PIN 3
-#define PINKY_PIN 4
-
 void setupServer()
 {
     // ------------------ File System ------------------
--- src/server.h
+++ src/server.h
@@ -2,27 +2,15 @@
 #define SERVER_H
 
 #include <SPIFFS.h> // Contrains FS.h
-#include "esp_camera.h"
 #include "ESPAsyncWebServer.h"
 
-#define LEFT_PWM_CHANNEL 2
-#define LEFT_MOTOR_EN 12
-#define LEFT_MOTOR_IN1 14
-#define LEFT_MOTOR_IN2 27
-
-#define RIGHT_PWM_CHANNEL 4
-#define RIGHT_MOTOR_EN 32
-#define RIGHT_MOTOR_IN1 33
-#define RIGHT_MOTOR_IN2 26
+#define THUMB_PIN 0
+#define INDEX_PIN 1
+#define MIDDLE_PIN 2
+#define RING_PIN 3
+#define PINKY_PIN 4
 
 
 void setupServer();
-void streamJpg(AsyncWebServerRequest *request);
-
-typedef struct
-{
-    camera_fb_t *fb;
-    size_t index;
-} camera_frame_t;
 
 #endif
