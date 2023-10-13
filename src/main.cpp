49598295ed4cc05d37fa298b730e78c3d4308718
SilvaUnCompte <silvablocks@gmail.com>
2023-10-13 10:24:22 +0200

    Tkt

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,82 @@
-#include <Arduino.h>
-#include "server.h"
+// #include <Arduino.h>
+// #include "server.h"
 
-// paramètres de votre réseau WiFi
-const char* ssid = "Esp32-SacroSaint-Wifi";
-const char* password = "patate12";
+// // paramètres de votre réseau WiFi
+// const char *ssid = "Esp32-SacroSaint-Wifi";
+// const char *password = "patate12";
 
-// Set your Static IP address
-IPAddress local_IP(192, 168, 0, 1);
-IPAddress gateway(192, 168, 0, 0);
-IPAddress subnet(255, 255, 255, 0);
+// // Set your Static IP address
+// IPAddress local_IP(192, 168, 0, 1);
+// IPAddress gateway(192, 168, 0, 0);
+// IPAddress subnet(255, 255, 255, 0);
 
-void setup()
-{
-  // ----------------- Serial -----------------
+// void setup()
+// {
+//   // ----------------- Serial -----------------
 
-  Serial.begin(115200);
+//   Serial.begin(115200);
 
-  // ------------------ Wifi ------------------
+//   // ------------------ Wifi ------------------
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(local_IP, gateway, subnet);
-  WiFi.softAP(ssid, password);
+//   WiFi.mode(WIFI_AP);
+//   WiFi.softAPConfig(local_IP, gateway, subnet);
+//   WiFi.softAP(ssid, password);
 
-  Serial.println("WiFi connected");
+//   Serial.println("WiFi connected");
 
-  Serial.println("IP:");
-  Serial.println(WiFi.localIP());
+//   Serial.println("IP:");
+//   Serial.println(WiFi.localIP());
 
-  // ---------------- SPIFFS -----------------
+//   // ---------------- SPIFFS -----------------
 
-  if (!SPIFFS.begin())
-  {
-    Serial.println("SPIFFS err");
-    return;
-  }
+//   if (!SPIFFS.begin())
+//   {
+//     Serial.println("SPIFFS err");
+//     return;
+//   }
 
-  File root = SPIFFS.open("/");
-  File file = root.openNextFile();
+//   File root = SPIFFS.open("/");
+//   File file = root.openNextFile();
 
-  while (file)
-  {
-    Serial.print("File: ");
-    Serial.println(file.name());
-    file.close();
-    file = root.openNextFile();
-  }
+//   while (file)
+//   {
+//     Serial.print("File: ");
+//     Serial.println(file.name());
+//     file.close();
+//     file = root.openNextFile();
+//   }
 
-  // --------- Server & Camera ---------------
+//   // --------- Server & Camera ---------------
 
-  setupServer();
+//   setupServer();
 
-  // --------------- Pin engine ---------------
+//   // --------------- Pin engine ---------------
 
-  pinMode(THUMB_PIN, OUTPUT);
-  pinMode(INDEX_PIN, OUTPUT);
-  pinMode(MIDDLE_PIN, OUTPUT);
-  pinMode(RING_PIN, OUTPUT);
-  pinMode(PINKY_PIN, OUTPUT);
+//   Serial.println("Setup done!");
+// }
 
-  // ledcSetup(THUMB_PWM_CHANNEL, 30000, 8);
-  // ledcAttachPin(THUMB_PIN, THUMB_PWM_CHANNEL);
-  // ledcSetup(INDEX_PWM_CHANNEL, 30000, 8);
-  // ledcAttachPin(INDEX_PIN, INDEX_PWM_CHANNEL);
-  // ledcSetup(MIDDLE_PWM_CHANNEL, 30000, 8);
-  // ledcAttachPin(MIDDLE_PIN, MIDDLE_PWM_CHANNEL);
-  // ledcSetup(RING_PWM_CHANNEL, 30000, 8);
-  // ledcAttachPin(RING_PIN, RING_PWM_CHANNEL);
-  // ledcSetup(PINKY_PWM_CHANNEL, 30000, 8);
-  // ledcAttachPin(PINKY_PIN, PINKY_PWM_CHANNEL);
+// void loop()
+// {
+//   vTaskDelay(5000 / portTICK_PERIOD_MS);
+// }
 
-  Serial.println("Setup done!");
+#include <ESP32Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(27);  // attaches the servo on pin 9 to the servo object
 }
 
-void loop() { vTaskDelay(5000 / portTICK_PERIOD_MS); }
+void loop() {
+  myservo.write(170);
+            // tell servo to go to position in variable 'pos'
+    delay(1000);                       // waits 15ms for the servo to reach the position
+
+    myservo.write(0);              // tell servo to go to position in variable 'pos'
+    delay(1000);                       // waits 15ms for the servo to reach the position
+  
+}
